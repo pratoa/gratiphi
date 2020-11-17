c74f0fc7741c0c707150b8279cb689f82ffbb33d
Andres Prato <prato.andres@gmail.com>
2020-11-17 11:54:33 -0500

    Amplify init, add auth.

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {StatusBar} from 'react-native'
+import Amplify from '@aws-amplify/core'
+import {Authenticator} from 'aws-amplify-react-native'
+import awsconfig from '../aws-exports'
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
+  },
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Authenticator usernameAttributes="email" />
+    </>
   );
 }
 
