d23c324211328d5987098037cb1b76bb9ab17aa3
Andres Prato <prato.andres@gmail.com>
2020-11-17 20:14:49 -0500

    Login works, and added some dependencies.

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
 import React from 'react'
-import {StatusBar} from 'react-native'
+import { StatusBar, Platform } from 'react-native'
 import Amplify from '@aws-amplify/core'
-import {Authenticator} from 'aws-amplify-react-native'
-import awsconfig from '../aws-exports'
+import { Authenticator } from 'aws-amplify-react-native'
+import awsconfig from './aws-exports'
+import {AmplifyTheme} from './src/components/AmplifyTheme'
 
 Amplify.configure({
   ...awsconfig,
   Analytics: {
-    disabled: true,
-  },
+    disabled: true
+  }
 })
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <Authenticator usernameAttributes="email" />
+      <Authenticator usernameAttributes="email" signUpConfig={signUpConfig}/>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password',
+    },
+  ],
+};
