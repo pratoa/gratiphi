13def60c455715eb01ce550d41e3ecc7321081c6
Andres Guevara <ajgc98@gmail.com>
2021-02-02 20:28:59 -0500

    Adding images to home sc4reen

--- App.js
+++ App.js
@@ -1,65 +1,75 @@
-import React, { useState, useEffect } from 'react'
-import { ActivityIndicator, View } from 'react-native'
-import Amplify, { Auth, Hub } from 'aws-amplify'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View } from "react-native";
+import Amplify, { Auth, Hub } from "aws-amplify";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import awsconfig from './aws-exports'
-import Welcome from './src/screens/login/Welcome'
-import BottomTabNavigator from './src/screens/BottomTabNavigator'
-import SignIn from './src/screens/login/SignIn'
-import SignUp from './src/screens/login/SignUp'
-import ConfirmSignUp from './src/screens/login/ConfirmSignUp'
-import Home from './src/screens/Home'
+import awsconfig from "./aws-exports";
+import Welcome from "./src/screens/login/Welcome";
+import BottomTabNavigator from "./src/screens/BottomTabNavigator";
+import SignIn from "./src/screens/login/SignIn";
+import SignUp from "./src/screens/login/SignUp";
+import ConfirmSignUp from "./src/screens/login/ConfirmSignUp";
+import Home from "./src/screens/Home";
 
 Amplify.configure({
   ...awsconfig,
   Analytics: {
-    disabled: true
-  }
-})
+    disabled: true,
+  },
+});
 
 function App() {
   const AuthenticationStack = createStackNavigator();
   const AppStack = createStackNavigator();
-  
-  const AuthenticationNavigator = props => {
+
+  const AuthenticationNavigator = (props) => {
     return (
       <AuthenticationStack.Navigator>
-        <AuthenticationStack.Screen name="Welcome" component={Welcome} options={{headerShown: false}}/>
-        <AuthenticationStack.Screen name="SignIn" 
-            options={{
-              headerShown:  true,
-              headerTitle: "Sign In",
-              headerBackTitle: "Back"
-            }}>
-          {screenProps => (
+        <AuthenticationStack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{ headerShown: false }}
+        />
+        <AuthenticationStack.Screen
+          name="SignIn"
+          options={{
+            headerShown: true,
+            headerTitle: "Sign In",
+            headerBackTitle: "Back",
+          }}
+        >
+          {(screenProps) => (
             <SignIn {...screenProps} updateAuthState={props.updateAuthState} />
           )}
         </AuthenticationStack.Screen>
-        <AuthenticationStack.Screen name="SignUp" component={SignUp} 
-            options={{
-              headerShown:  true,
-              headerTitle: "Sign Up",
-              headerBackTitle: "Back"
-            }}>
-        </AuthenticationStack.Screen>
-        <AuthenticationStack.Screen name="ConfirmationSignUp" component={ConfirmSignUp} 
-            options={{
-              headerShown:  true,
-              headerTitle: "Sign In",
-              headerBackTitle: "Back"
-            }}>
-        </AuthenticationStack.Screen>
+        <AuthenticationStack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{
+            headerShown: true,
+            headerTitle: "Sign Up",
+            headerBackTitle: "Back",
+          }}
+        ></AuthenticationStack.Screen>
+        <AuthenticationStack.Screen
+          name="ConfirmationSignUp"
+          component={ConfirmSignUp}
+          options={{
+            headerShown: true,
+            headerTitle: "Sign In",
+            headerBackTitle: "Back",
+          }}
+        ></AuthenticationStack.Screen>
       </AuthenticationStack.Navigator>
     );
   };
-  
-  const AppNavigator = props => {
+
+  const AppNavigator = (props) => {
     return (
       <AppStack.Navigator>
         <AppStack.Screen name="Home">
-          {screenProps => (
+          {(screenProps) => (
             <Home {...screenProps} updateAuthState={props.updateAuthState} />
           )}
         </AppStack.Screen>
@@ -69,12 +79,12 @@ function App() {
 
   const Initializing = () => {
     return (
-      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator size="large" color="tomato" />
       </View>
     );
   };
-  let [isUserLoggedIn, setUserLoggedIn] = useState('initializing');
+  let [isUserLoggedIn, setUserLoggedIn] = useState("initializing");
 
   useEffect(() => {
     checkAuthState();
@@ -82,33 +92,33 @@ function App() {
 
   async function checkAuthState() {
     try {
-		await Auth.currentAuthenticatedUser();
-		console.log('✅ User is signed in');
-		setUserLoggedIn('loggedIn');
+      await Auth.currentAuthenticatedUser();
+      console.log("✅ User is signed in");
+      setUserLoggedIn("loggedIn");
     } catch (err) {
-		console.log(err);
-		console.log('❌ User is not signed in');
-		setUserLoggedIn('loggedOut');
-		return null;
+      console.log(err);
+      console.log("❌ User is not signed in");
+      setUserLoggedIn("loggedOut");
+      return null;
     }
   }
 
   function updateAuthState(isUserLoggedIn) {
-	  setUserLoggedIn(isUserLoggedIn);
+    setUserLoggedIn(isUserLoggedIn);
   }
 
   return (
     <NavigationContainer>
-      {isUserLoggedIn === 'initializing' && <Initializing />}
-      {isUserLoggedIn === 'loggedIn' && (
+      {isUserLoggedIn === "initializing" && <Initializing />}
+      {isUserLoggedIn === "loggedIn" && (
         <BottomTabNavigator updateAuthState={updateAuthState} />
       )}
-      {isUserLoggedIn === 'loggedOut' && (
+      {isUserLoggedIn === "loggedOut" && (
         <AuthenticationNavigator updateAuthState={updateAuthState} />
       )}
     </NavigationContainer>
   );
-};
+}
 
 export default App;
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,79 +1,114 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
-import { Auth } from 'aws-amplify';
-import Carousel, { Pagination } from 'react-native-snap-carousel';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  FlatList,
+  Dimensions,
+} from "react-native";
+import { Auth } from "aws-amplify";
+import Carousel, { Pagination } from "react-native-snap-carousel";
 
-const SLIDER_WIDTH = Dimensions.get('window').width;
+const SLIDER_WIDTH = Dimensions.get("window").width;
 const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.8);
 const DATA = [];
 
 class InformationItem {
-    constructor(title, message, image, type) {
-        this.title = title;
-        this.message = message;
-		this.image = image;
-		this.type = type;
-    }
+  constructor(title, message, image, type) {
+    this.title = title;
+    this.message = message;
+    this.image = image;
+    this.type = type;
+  }
 }
-const comida = require('../../assets/images/comida_logo.png');
-const item1 = new InformationItem('1 plate of food = 0.6 USD', 'Feed a child for 1 month: 12 USD', comida, 'image');
+const comida = require("../../assets/images/comida_logo.png");
+const item1 = new InformationItem(
+  "1 plate of food = 0.6 USD",
+  "Feed a child for 1 month: 12 USD",
+  comida,
+  "image"
+);
 var item2 = {
-	'title': '+ 907',
-	'message': 'Volunteer mothers'
-}
+  title: "+ 907",
+  message: "Volunteer mothers",
+};
 var item3 = {
-	'title': '+ 907',
-	'message': 'Volunteer mothers'
-}
+  title: "+ 907",
+  message: "Volunteer mothers",
+};
 var item4 = {
-	'title': '+ 907',
-	'message': 'Volunteer mothers'
-}
-DATA.push(item1)
-DATA.push(item2)
-DATA.push(item3)
-DATA.push(item4)
+  title: "+ 907",
+  message: "Volunteer mothers",
+};
+DATA.push(item1);
+DATA.push(item2);
+DATA.push(item3);
+DATA.push(item4);
+
+const images = [
+  {
+    id: 1,
+    url: require("./../../assets/images/food_security.png"),
+  },
+  {
+    id: 2,
+    url: require("./../../assets/images/education.png"),
+  },
+  {
+    id: 3,
+    url: require("./../../assets/images/health.png"),
+  },
+  {
+    id: 4,
+    url: require("./../../assets/images/training.png"),
+  },
+  {
+    id: 5,
+    url: require("./../../assets/images/family.png"),
+  },
+];
 
 export default function Home() {
-	let [index, setIndex] = useState(0);
-    
-    function renderItem({ item, index }) {
-		if (item.image) {
-			return (
-				<View style={styles.itemContainer}>
-					<Image source={item.image} style={styles.image}/>
-					<Text style={styles.itemTitle}>{item.title}</Text>
-			  	</View>	
-			)
-		}
+  let [index, setIndex] = useState(0);
 
-        return (
-          <View style={styles.itemContainer}>
-            <Text style={styles.itemLabel}>{item.title}</Text>
-			<Text style={styles.itemLabel}>{item.message}</Text>
-          </View>
-        );
-	}
+  function renderItem({ item, index }) {
+    if (item.image) {
+      return (
+        <View style={styles.itemContainer}>
+          <Image source={item.image} style={styles.image} />
+          <Text style={styles.itemTitle}>{item.title}</Text>
+        </View>
+      );
+    }
 
     return (
-		<View style={styles.mainContainer}>
-			<View style={styles.container}>
-				<Carousel
-					data={DATA}
-					renderItem={renderItem}
-					sliderWidth={SLIDER_WIDTH}
-					itemWidth={ITEM_WIDTH}
-					layout={'default'}
-					contentContainerCustomStyle={styles.carouselContainer}
-					onSnapToItem={(index) => setIndex(index) }
-					loop={true}
-					autoplay={true}
-					// enableMomentum={false}
-					// lockScrollWhileSnapping={true}
-					autoplayDelay={1000}
-					autoplayInterval={3000}
-				/>
-				{/* <Pagination 
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemLabel}>{item.title}</Text>
+        <Text style={styles.itemLabel}>{item.message}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <Carousel
+          data={DATA}
+          renderItem={renderItem}
+          sliderWidth={SLIDER_WIDTH}
+          itemWidth={ITEM_WIDTH}
+          layout={"default"}
+          contentContainerCustomStyle={styles.carouselContainer}
+          onSnapToItem={(index) => setIndex(index)}
+          loop={true}
+          autoplay={true}
+          // enableMomentum={false}
+          // lockScrollWhileSnapping={true}
+          autoplayDelay={1000}
+          autoplayInterval={3000}
+        />
+        {/* <Pagination 
 					dotsLength={DATA.length}
 					activeDotIndex={index}
 					containerStyle={{  }}
@@ -87,54 +122,82 @@ export default function Home() {
 					inactiveDotOpacity={0.4}
 					inactiveDotScale={0.6}
 					/>	 */}
-			</View>
-			<View style={styles.secondContainer}>
-			</View>	
-		</View>
-    );
+      </View>
+
+      <View style={styles.secondContainer}>
+        <FlatList
+          data={images}
+          numColumns={3}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Image
+              style={styles.icons}
+              resizeMode="center"
+              source={item.url}
+            ></Image>
+          )}
+        ></FlatList>
+      </View>
+      <View style={styles.thirdContainer}></View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	mainContainer: {
-		flex: 1
-	},
-    container: {
-        flex: 1,
-        alignItems: 'center',
-		backgroundColor: '#325e9d', //'#325e9d'
-		alignContent: 'center',
-		justifyContent: 'center'
-	},
-	secondContainer: {
-		flex: 3
-	},
-	carouselContainer: {
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-    itemContainer: {
-        width: ITEM_WIDTH,
-        height: '90%',
-        alignItems: 'center',
-        justifyContent: 'center',
-		backgroundColor: '#f2c300',
-		borderRadius: 20
-	},
-	itemLabel: {
-		color: 'white',
-		fontSize: 24
-	},
-	itemTitle: {
-		marginTop: 15,
-		flex: 1,
-		color: 'white',
-		fontSize: 24
-	},
-	image: {
-		marginTop: 15,
-		flex: 1,
-		width: 30,
-		height: 30,
-		resizeMode: 'contain'
-	},
+  mainContainer: {
+    flex: 1,
+  },
+  container: {
+    flex: 2,
+    alignItems: "center",
+    backgroundColor: "#325e9d", //'#325e9d'
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  secondContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 3,
+    backgroundColor: "white",
+  },
+  thirdContainer: {
+    backgroundColor: "grey",
+    flex: 2,
+  },
+  icons: {
+    width: 110,
+    height: 110,
+    padding: 15,
+    margin: 5,
+    backgroundColor: "white",
+  },
+  carouselContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    width: ITEM_WIDTH,
+    height: "90%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f2c300",
+    borderRadius: 20,
+  },
+  itemLabel: {
+    color: "white",
+    fontSize: 24,
+  },
+  itemTitle: {
+    marginTop: 15,
+    flex: 1,
+    color: "white",
+    fontSize: 24,
+  },
+  image: {
+    marginTop: 15,
+    flex: 1,
+    width: 30,
+    height: 30,
+    resizeMode: "contain",
+  },
 });
