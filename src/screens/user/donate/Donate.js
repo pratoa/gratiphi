0d18b1aab2fdad8b4820bb8af576e1d83d3dfa0b
Andres Prato <prato.andres@gmail.com>
2021-04-27 15:37:00 -0400

    GR-3: Remove fixed array.

--- src/screens/user/donate/Donate.js
+++ src/screens/user/donate/Donate.js
@@ -8,57 +8,6 @@ import Screen from "../../../components/common/Screen";
 
 const SLIDER_WIDTH = Dimensions.get("window").width;
 const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.8);
-const DATA = [];
-DATA.push(
-  new DoneeItem(
-    "Austin",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    require("../../../../assets/images/donees/austin-nicomedez.jpg"),
-    8,
-    "La Vega, Caracas"
-  )
-);
-DATA.push(
-  new DoneeItem(
-    "Katie",
-    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    require("../../../../assets/images/donees/katie-moum.jpg"),
-    10,
-    "Portuguesa, Venezuela"
-  )
-);
-DATA.push(
-  new DoneeItem(
-    "Muhammad",
-    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    require("../../../../assets/images/donees/muhammad-muzamil.jpg"),
-    7,
-    "Petare, Venezuela"
-  )
-);
-DATA.push(
-  new DoneeItem(
-    "Muhammad",
-    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    require("../../../../assets/images/donees/muhammad-taha-khan.jpg"),
-    11,
-    "Coro, Falcon"
-  )
-);
-DATA.push(
-  new DoneeItem(
-    "Nivedita",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    require("../../../../assets/images/donees/nivedita-singh.jpg"),
-    6,
-    "Sucre, Venezuela"
-  )
-);
 
 export default function Donate({ props }) {
   // const [modalVisible, setModalVisible] = useState(false);
