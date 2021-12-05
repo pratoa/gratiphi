0450e3ae301273d965d168ee5172fbc65e5afa6f
Andres Prato <prato.andres@gmail.com>
2021-12-05 16:02:06 -0500

    After confirmation signUp, sign in the user.

--- src/screens/login/ConfirmSignUp.js
+++ src/screens/login/ConfirmSignUp.js
@@ -7,16 +7,29 @@ import AppButton from "../../components/common/AppButton";
 import colors from "../../config/colors";
 import Screen from "../../components/common/Screen";
 
-export default function ConfirmSignUp({ route, navigation }) {
+export default function ConfirmSignUp({
+  route,
+  navigation,
+  updateAuthState,
+  updateUserGroup,
+}) {
   const email = route.params.email;
+  const password = route.params.password;
   const [authCode, setAuthCode] = useState("");
   const [error, setError] = useState("");
 
   async function confirmSignUp() {
     try {
-      await Auth.confirmSignUp(email, authCode);
-      console.log("Code Confirmed");
-      navigation.replace("SignIn");
+      var confirmPromise = await Auth.confirmSignUp(email, authCode);
+      if (confirmPromise == "SUCCESS") {
+        let user = await Auth.signIn(email, password);
+        updateUserGroup(
+          user.signInUserSession.accessToken.payload["cognito:groups"][0]
+        );
+        updateAuthState("loggedIn");
+        console.log("Code Confirmed");
+        navigation.replace("Main");
+      }
     } catch (error) {
       setError(error);
       console.log("Verification code does not match.", error.code);
