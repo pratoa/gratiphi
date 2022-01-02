8227b4050238fc0782366665a4e3b9ba9252af35
andresjguevara <49961335+andresjguevara@users.noreply.github.com>
2022-01-02 09:39:19 -0500

    Features/settings (#21)
    * Adding seeting functionality for Account settings
    
    * Basic implementation of About Us and T&A
    
    * Adding T&A and About Us

--- src/screens/user/settings/SettingsTandA.js
+++ src/screens/user/settings/SettingsTandA.js
@@ -1,13 +1,44 @@
-import React, { useEffect, useState } from "react";
-import { Auth } from "aws-amplify";
-import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { StyleSheet, Linking, FlatList } from "react-native";
 
-import AppTextInput from "../../../components/common/AppTextInput";
-import AppButton from "../../../components/common/AppButton";
-import colors from "../../../config/colors";
+import ListItemSeparator from "../../../components/common/ListItemSeparator";
+import ListItemComponent from "../../../components/common/ListItemComponent";
 import Screen from "../../../components/common/Screen";
 import { default as defaultStyle } from "../../../config/styles";
 
+const SETTINGS_T_AND_A = [
+  {
+    title: "Privacy Policy",
+    uri: "https://gratification-proof114704-dev.s3.amazonaws.com/public/resources/Gratiphi+Test+Privacy+Policy.pdf",
+  },
+  {
+    title: "Terms and Agreement",
+    uri: "https://gratification-proof114704-dev.s3.amazonaws.com/public/resources/Gratiphi+Test+T%26A.pdf",
+  },
+];
+
 export default function SettingsTandA({ navigation, updateAuthState }) {
-  return <Screen></Screen>;
+  return (
+    <Screen style={styles.container}>
+      <FlatList
+        data={SETTINGS_T_AND_A}
+        keyExtractor={(option) => option.title}
+        renderItem={({ item }) => (
+          <ListItemComponent
+            title={item.title}
+            onPress={() => {
+              Linking.openURL(item.uri);
+            }}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+      />
+    </Screen>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
